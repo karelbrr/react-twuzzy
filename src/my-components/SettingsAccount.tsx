e65fa3873788a38bfd81c5fcd02896f377e3d243
karel.braborec <karel.braborec@office365.spspzlin.cz>
2024-11-29 11:05:38 +0100

    uprava respo a pridani inputs do settings account

--- src/my-components/SettingsAccount.tsx
+++ src/my-components/SettingsAccount.tsx
@@ -1,7 +1,23 @@
+import { useAuth } from "@/auth/AuthProvider";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export const SettingsAccount = () => {
+  const { user } = useAuth();
+
   return (
     <section className="">
       <h2 className="font-bold text-3xl m-10">Account Settings</h2>
+      <form className="space-y-5">
+        <div className="flex flex-col w-1/3 gap-1.5 ml-10">
+          <Label htmlFor="provider">Provider</Label>
+          <Input id="provider" placeholder="GitHub" disabled />
+        </div>
+        <div className="flex flex-col w-1/3 gap-1.5 ml-10">
+          <Label htmlFor="provider">Email</Label>
+          <Input id="email" placeholder={user?.email} disabled />
+        </div>
+      </form>
     </section>
   );
 };
--- src/pages/ProfileEdit.tsx
+++ src/pages/ProfileEdit.tsx
@@ -3,26 +3,25 @@ import { UserPen } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ShieldAlert } from "lucide-react";
 import { Key } from "lucide-react";
-import { ExternalLink } from "lucide-react";
+import { MoveLeft } from "lucide-react";
 
 export const ProfileEdit = () => {
   return (
     <section className=" h-screen flex ">
       <div className=" w-1/6 border-r   ">
-        <div className="border-b mt-6 pb-6 pl-8 flex items-baseline">
-          <h3 className=" font-bold text-2xl m-auto ">Settings</h3>
-          <Button asChild variant={"outline"} className="mr-5">
+        <div className=" mt-6 pb-6 flex border-b items-baseline">
+          <Button asChild variant={"outline"} className="ml-8">
             <Link to={"/"}>
-              <ExternalLink />
-              Chats
+              <MoveLeft />
             </Link>
           </Button>
+          <h3 className=" font-bold text-2xl  ml-5">Settings</h3>
         </div>
-        <div className="flex flex-col ml-10 m-auto mt-5 font-extralight">
-          <div className="flex flex-col w-5/6  space-y-1">
+        <div className="flex flex-col ml-10 xl:ml-20 m-auto mt-5 font-extralight">
+          <div className="flex  flex-col w-5/6  space-y-1">
             <Link
               to={"/settings/account"}
-              className="px-5   font-medium  hover:underline hover:underline-offset-4 items-baseline text-left  flex w-full text-sm"
+              className="px-5 xl:text-[15px] font-medium  hover:underline hover:underline-offset-4 items-baseline text-left  flex w-full text-sm"
             >
               <Key size={15} className="mr-1" />
               Account
@@ -30,7 +29,7 @@ export const ProfileEdit = () => {
 
             <Link
               to={"/settings/profile"}
-              className="px-5   font-medium  hover:underline hover:underline-offset-4 items-baseline text-left  flex w-full text-sm"
+              className="px-5 xl:text-[15px] font-medium  hover:underline hover:underline-offset-4 items-baseline text-left  flex w-full text-sm"
             >
               <UserPen size={15} className="mr-1 " />
               Profile
@@ -38,7 +37,7 @@ export const ProfileEdit = () => {
 
             <Link
               to={"/settings/blocked"}
-              className="px-5   font-medium  hover:underline hover:underline-offset-4 items-baseline text-left text-sm flex w-full"
+              className="px-5 xl:text-[15px] font-medium  hover:underline hover:underline-offset-4 items-baseline text-left text-sm flex w-full"
             >
               <ShieldAlert size={15} className="mr-1" />
               Blocked People
